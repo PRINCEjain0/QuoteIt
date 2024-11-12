770da911e15e7366b14bd85b8c3d66717a15233d
Prince Jain <pj122333444@gmail.com>
2024-11-12 18:55:55 +0530

    fixed ui2

--- app/page.jsx
+++ app/page.jsx
@@ -1,58 +1,36 @@
 "use client";
 import Navbar from "../components/Navbar";
-import Stories from "../components/Stories";
 import PostCard from "../components/PostCard";
-import {
-  HomeIcon,
-  BookmarkIcon,
-  PlusIcon,
-  UserIcon,
-  MapPinIcon,
-} from "@heroicons/react/24/outline";
-
 import { useRouter } from "next/navigation";
-
-
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const router = useRouter();
+  const { status } = useSession();
 
-  const profile = () => {
-
-    router.push("/profile");
-
-  }
+  const handleAddPost = () => {
+    if (status === "authenticated") {
+      router.push("/profile");
+    } else {
+      // Redirect to login page or show an error message
+      console.log("User is not authenticated");
+    }
+  };
 
   return (
-    <>
-      <div className="min-h-screen bg-gray-100">
-        <div className="container mx-auto px-4">
-          <Navbar />
-
-          <PostCard />
-        </div>
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto px-4">
+        <Navbar />
+        <PostCard />
+        {status === "authenticated" && (
+          <button
+            className="fixed bottom-0 left-4 right-4 bg-[#F9F16F] px-6 py-3 flex justify-center items-center rounded-md shadow-md text-[#3A1B0F] font-medium sm:hidden"
+            onClick={handleAddPost}
+          >
+            Add Post
+          </button>
+        )}
       </div>
-      <button className="fixed bottom-0  bg-[#F9F16F] w-full flex justify-center rounded p-4  sm:hidden" onClick={profile}>
-        {/* <button className="p-2 rounded" aria-label="Home">
-          <HomeIcon className="w-6 h-6 text-[#3A1B0F] fill-current" />
-        </button>
-        <button className="p-2 rounded" aria-label="User">
-          <UserIcon className="w-6 h-6 text-[#3A1B0F] fill-current" />
-        </button>
-        <button className="p-2 rounded" aria-label="Add">
-          <PlusIcon className="w-6 h-6 text-[#3A1B0F] fill-current" />
-        </button>
-        <button className="p-2 rounded" aria-label="Bookmark">
-          <BookmarkIcon className="w-6 h-6 text-[#3A1B0F] fill-current" />
-        </button>
-        <button className="p-2 rounded" aria-label="Location">
-          <MapPinIcon className="w-6 h-6 text-[#3A1B0F] fill-current" />
-        </button> */}
-
-        {/* <button className="bg-[#F9F16F] text-[#3A1B0F] px-4 py-2 rounded font-medium" aria-label="User" onClick={profile}> */}
-        Add Post
-        {/* </button> */}
-      </button>
-    </>
+    </div>
   );
 }
